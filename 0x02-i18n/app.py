e196344ac48b6940544e07669cc0f433a963ea87
CheropS <sh.cherop@gmail.com>
2023-08-08 13:49:27 +0300

    add missing files

---
+++ 0x02-i18n/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A flask to infer appropriate time zone
+"""
+import pytz
+from typing import Union, Dict
+from flask_babel import Babel, format_datetime
+from flask import Flask, render_template, request, g
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
